37e9a6f5722d99f3332421afb8d092abd232e806
Drew Heavner <drew.heavner@reddit.com>
2024-08-17 01:19:53 -0400

    Removed old template code

--- sample/shared/src/androidMain/kotlin/com/r0adkll/kimchi/restaurant/Platform.android.kt
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2024 r0adkll
-// SPDX-License-Identifier: Apache-2.0
-package com.r0adkll.kimchi.restaurant
-
-class AndroidPlatform : Platform {
-  override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
