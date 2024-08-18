2f84d27fbb4106370d832624f05ebe77200a5217
Drew Heavner <drew.heavner@reddit.com>
2024-08-18 16:57:46 -0400

    Add js/wasm targets to annotation modules

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
 // Copyright (C) 2024 r0adkll
 // SPDX-License-Identifier: Apache-2.0
 plugins {
@@ -17,15 +19,22 @@ kotlin {
   iosSimulatorArm64()
   macosX64()
   macosArm64()
+  js(IR) {
+    moduleName = property("POM_ARTIFACT_ID").toString()
+    browser()
+  }
+  @OptIn(ExperimentalWasmDsl::class)
+  wasmJs {
+    moduleName = property("POM_ARTIFACT_ID").toString()
+    browser()
+  }
 
   sourceSets {
     commonMain.dependencies {
       api(libs.kotlininject.runtime)
     }
-    commonTest.dependencies {
-      implementation(libs.kotlin.test)
-    }
   }
 }
 
 android { namespace = "com.r0adkll.kimchi.annotations" }
+androidComponents { beforeVariants { variant -> variant.androidTest.enable = false } }
--- circuit/annotations/build.gradle.kts
+++ circuit/annotations/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
 // Copyright (C) 2024 r0adkll
 // SPDX-License-Identifier: Apache-2.0
 plugins {
@@ -17,6 +19,15 @@ kotlin {
   iosSimulatorArm64()
   macosX64()
   macosArm64()
+  js(IR) {
+    moduleName = property("POM_ARTIFACT_ID").toString()
+    browser()
+  }
+  @OptIn(ExperimentalWasmDsl::class)
+  wasmJs {
+    moduleName = property("POM_ARTIFACT_ID").toString()
+    browser()
+  }
 
   sourceSets {
     commonMain.dependencies {
@@ -24,10 +35,8 @@ kotlin {
       compileOnly(libs.circuit.foundation)
       compileOnly(libs.kotlininject.runtime)
     }
-    commonTest.dependencies {
-      implementation(libs.kotlin.test)
-    }
   }
 }
 
 android { namespace = "com.r0adkll.kimchi.circuit.annotations" }
+androidComponents { beforeVariants { variant -> variant.androidTest.enable = false } }
